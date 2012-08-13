4a0e9833787cd1882da82162e8a421f3b5e4cd09
Zac Stewart <zgstewart@gmail.com>
2012-08-13 10:55:40 -0400

    Set mobile session and add mobile content-type
    This way you don't have to always have mobile=x in the url. Just do it
    once, and then you can unmobilize by setting it back to 0.
    
    The mobile content-type is inert. It doesn't actually do anything, but
    it puts it in place so that you can respond with different formats in
    `respond_to` if you choose to.

--- lib/mobilicious/rails_ext.rb
+++ lib/mobilicious/rails_ext.rb
@@ -1,22 +1,26 @@
 require 'action_pack'
 
 module MightBeMobile
-
   MOBILE_REGEX = /mobile/i
 
   def is_mobile?
-    return true if params[:mobile].present?
-    return request.headers['User-Agent'] =~ MOBILE_REGEX
+    if session[:mobile_param]
+      session[:mobile_param] == '1'
+    else
+      request.user_agent =~ MOBILE_REGEX
+    end
   end
 
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if is_mobile?
+  end
 end
-
 
 ActionController::Base.instance_eval do
   def might_be_mobile
     include MightBeMobile
     helper_method :is_mobile?
+    before_filter :prepare_for_mobile
   end
 end
-
-
